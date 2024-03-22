a96b5755b31d81006164fb2e81e3a8927969e2b1
tobitprince <prince.tobit@strathmore.edu>
2024-03-22 09:46:50 +0300

    Add CRUD operations for product management

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product')
 
-//create new product
+//create new product /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
 
     const product = await Product.create(req.body);
@@ -11,10 +11,75 @@ exports.newProduct = async (req, res, next) => {
     })
 }
 
+// get all products in the database
+exports.getProducts = async (req, res, next) => {
 
-exports.getProducts = (req, res, next) => {
+    const products = await Product.find();
     res.status(200).json({
         succes: true,
-        message: 'This route will show all products in the database.'
+        count: products.length,
+        products
+    })
+}
+
+//get single product details  /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id)
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//update product   /api/v1/admin/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//Delete Product /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted'
     })
 }
