a655ac8f33ec7ded1d11c347612f3bf5cc995947
tobitprince <prince.tobit@strathmore.edu>
2024-03-26 21:34:48 +0300

    Add pagination feature to productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,14 +22,19 @@ exports.newProduct = catchAsyncErrors (async (req, res, next) => {
 // get all products in the database
 exports.getProducts = catchAsyncErrors ( async (req, res, next) => {
 
+    const resPerPage = 4;
+    const productCount = await Product.countDocuments();
+
 
     const apiFeatures = new APIFeatures(Product.find(), req.query)
                         .search()
                         .filter()
+                        .pagination(resPerPage)
     const products = await apiFeatures.query;
     res.status(200).json({
         succes: true,
         count: products.length,
+        productCount,
         products
     })
 })
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -38,6 +38,14 @@ class APIFeatures {
         return this;
 
     }
+
+    pagination(resPerPage) {
+        const currentPage = Number(this.queryString.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+        
+        this.query = this.query.limit(resPerPage).skip(skip);
+        return this;
+    }
 }
 
 module.exports = APIFeatures
