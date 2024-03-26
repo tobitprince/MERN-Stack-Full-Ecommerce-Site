4cdcf654241dbde4a0e20d1257815648f1021a4a
tobitprince <prince.tobit@strathmore.edu>
2024-03-26 21:24:24 +0300

    Add filter functionality to APIFeatures class

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,6 +25,7 @@ exports.getProducts = catchAsyncErrors ( async (req, res, next) => {
 
     const apiFeatures = new APIFeatures(Product.find(), req.query)
                         .search()
+                        .filter()
     const products = await apiFeatures.query;
     res.status(200).json({
         succes: true,
