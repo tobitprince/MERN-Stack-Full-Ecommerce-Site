14ed221f0032b97c79e112a3ad8e502ed59793c8
tobitprince <prince.tobit@strathmore.edu>
2024-03-26 20:41:35 +0300

    Validation & Mongose ID Error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -15,9 +15,21 @@ module.exports = (err, req  ,res, next) => {
     }
 
     if(process.env.NODE_ENV === 'PRODUCTION') {
-        let error = {...err}
+        let error = { ...err }
 
-        error.message = err.message
+        error.message = err.message;
+
+        // Wrong Mongoose Object ID Error
+        if(err.name === 'CastError') {
+            const message = `Resource not found. Invalid: ${err.path}`
+            error = new ErrorHandler(message, 400)
+        }
+
+        // Handling Mongoose Validation Error
+        if(err.name === 'ValidationError') {
+            const message = Object.values(err.errors).map(value => value.message);
+            error = new ErrorHandler(message, 400)
+        }
 
         res.status(error.statusCode).json({
             success: false,
