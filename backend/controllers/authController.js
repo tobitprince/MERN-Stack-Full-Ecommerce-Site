96d73711e24a4b3a9287a07c2c6381120ebe556f
tobitprince <prince.tobit@strathmore.edu>
2024-03-30 19:14:26 +0300

    Sending JWT Token in Cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,8 @@
-const User = require('../models/user')
+const User = require('../models/user');
 
-const ErrorHandler = require('../utils/errorHandler')
-const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 // register user =? /api/v1/register
 exports.registerUser = catchAsyncErrors ( async (req, res ,next) => {
@@ -17,15 +18,10 @@ exports.registerUser = catchAsyncErrors ( async (req, res ,next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
 })
 
-//login user => /api/v1/login
+//login user => /api/v1/login 
 
 exports.loginUser = catchAsyncErrors ( async (req, res, next) => {
     const { email, password } = req.body;
@@ -47,10 +43,6 @@ exports.loginUser = catchAsyncErrors ( async (req, res, next) => {
         return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
-    const token = user.getJwtToken();
+    sendToken(user, 200, res)
 
-    res.status(200).json({
-        success: true,
-        token
-    })
 })
