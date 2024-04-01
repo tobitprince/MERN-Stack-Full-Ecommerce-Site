402fed652b8736281f0290afb3445ba344f91ce8
tobitprince <prince.tobit@strathmore.edu>
2024-04-01 21:28:05 +0300

    added file to gitignore

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,6 +4,7 @@ const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');  
 
 // register user =? /api/v1/register
 exports.registerUser = catchAsyncErrors ( async (req, res ,next) => {
@@ -86,6 +87,35 @@ exports.forgotPassword = catchAsyncErrors( async(req, res, next) => {
         return next(new ErrorHandler(error.message, 500))
     }
 
+})
+
+//reset password => /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors( async(req, res, next) => {
+    //hash url token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now()}
+    })
+
+    if(!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if(req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    //setup new password
+    user.password = req.body.password;
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res)
 })
 
 
