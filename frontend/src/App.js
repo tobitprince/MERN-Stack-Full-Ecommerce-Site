6b38a43570f0f4607f11ab0a6fc3c333dcd72c17
tobitprince <prince.tobit@strathmore.edu>
2024-04-03 22:48:22 +0300

    Created Header and Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer';
 function App() {
   return (
     <div className="App">
-      <h1>Nunua</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
