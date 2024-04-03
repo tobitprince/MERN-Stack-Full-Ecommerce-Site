a7863d68300d38fc63be8fca02a41560339cb4e4
tobitprince <prince.tobit@strathmore.edu>
2024-04-04 01:02:08 +0300

    Implented Redux and changed app dependecies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Header from './components/layout/Header'
+import Home from './components/Home';
 import Footer from './components/layout/Footer';
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className='container container-fluid'>
+          <Routes>
+              <Route path="/" element={<Home />} exact />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
